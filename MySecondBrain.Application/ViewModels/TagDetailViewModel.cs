a70768037d76fcb60c94deb3d2fae382572df644
Laurane <laurane.mh@gmail.com>
2022-08-19 17:02:57 +0200

    Add CRUD Tag

---
+++ MySecondBrain.Application/ViewModels/TagDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySecondBrain.Infrastructure.DB;
+
+namespace MySecondBrain.Application.ViewModels
+{
+    public class TagDetailViewModel
+    {
+        public Tag Tag { get; set; }
+    }
+}
--- MySecondBrain.Domain/Services/TagService.cs
+++ MySecondBrain.Domain/Services/TagService.cs
@@ -3,11 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySecondBrain.Infrastructure.DB;
 
 namespace MySecondBrain.Domain.Services
 {
-    class TagService
+    public class TagService
     {
-        
+        public static List<Tag> GetTags()
+        {
+            MySecondBrain_LMContext mySecondBrainContext = new MySecondBrain_LMContext();
+
+            List<Tag> tag1 = new List<Tag>();
+
+            var tags = mySecondBrainContext.Tags.ToList();
+
+            foreach (var getTag in tags)
+            {
+                Tag tag = new Tag();
+
+                tag.Idtag = getTag.Idtag;
+                tag.Nom = getTag.Nom;
+                tag.UserId = getTag.UserId;
+                tag1.Add(tag);
+            }
+
+            return tag1;
+        }
+        public static Tag GetTag(int tagId)
+        {
+            using (MySecondBrain_LMContext db = new MySecondBrain_LMContext())
+            {
+                return db.Tags.Find(tagId);
+            }
+        }
+
+        public static List<Tag> GetAllTagsOfUser(string userId)
+        {
+            using (MySecondBrain_LMContext db = new MySecondBrain_LMContext())
+            {
+                return db.Tags.Where(n => n.User.Id == userId).ToList();
+            }
+        }
+
+        public static void CreateTag(Tag tag, string userId)
+        {
+            tag.UserId = userId;
+
+            MySecondBrain_LMContext db = new MySecondBrain_LMContext();
+
+            db.Tags.Add(tag);
+            db.SaveChanges();
+        }
+
+        public static void EditTag(Tag tag)
+        {
+            MySecondBrain_LMContext db = new MySecondBrain_LMContext();
+
+            Tag tagToUpdate = db.Tags.Find(tag.Idtag);
+            {
+                if (tagToUpdate != null)
+                {
+                    tagToUpdate.Nom = tag.Nom;
+                    db.Update(tagToUpdate);
+                }
+            }
+            db.SaveChanges();
+        }
+
+
+        public static void DeleteTag(int tagId)
+        {
+            MySecondBrain_LMContext db = new MySecondBrain_LMContext();
+
+            var tagToRemove = db.Tags.SingleOrDefault(a => a.Idtag == tagId);
+            if (tagToRemove != null)
+            {
+                db.Tags.Remove(tagToRemove);
+                db.SaveChanges();
+            }
+
+        }
     }
 }
